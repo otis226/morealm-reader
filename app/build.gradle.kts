bc00146288f176cc55a6fe15cc1445378b79d7ed
otis226 <otis22699@gmail.com>
2026-08-30 01:34:18 +0700

    Bump Story Reader to v0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.otis.edgereader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 7
+        versionName = "0.7.0"
     }
 
     compileOptions {
